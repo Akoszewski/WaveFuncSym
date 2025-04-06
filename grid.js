23d52342bc2939adcdc1844e429fb8a25eeb6d4d
Akoszewski <akoszews@gmail.com>
2025-04-06 11:40:52 +0200

    Convertion from 2 dimentional array to one dimentional

--- grid.js
+++ grid.js
@@ -15,34 +15,34 @@ function setPixel(ctx, pixelBuffer, x, y, color = [0, 0, 0, 255])
     pixelBuffer.data[index + 3] = color[3]; // A
 }
 
-function initializeRandomNoise(width, height)
-{
-    const psi = new Array(height).fill(0).map(() => new Array(width).fill(0));
-    for (let y = 0; y < height; y++)
-    {
-        for (let x = 0; x < width; x++)
-        {
-            psi[y][x] = roundComplex(math.complex(Math.random() - 0.5, Math.random() - 0.5), 3);
-        }
-    }
-    return psi;
-}
+// function initializeRandomNoise(width, height)
+// {
+//     const psi = new Array(height).fill(0).map(() => new Array(width).fill(0));
+//     for (let y = 0; y < height; y++)
+//     {
+//         for (let x = 0; x < width; x++)
+//         {
+//             psi[y][x] = roundComplex(math.complex(Math.random() - 0.5, Math.random() - 0.5), 3);
+//         }
+//     }
+//     return psi;
+// }
 
-function initializeGaussianWavepacket(width, height, center_x, center_y, sigma, init_momemtum_x, init_momentum_y, scalingConstant)
+function initializeGaussianWavepacket(w, h, center_x, center_y, sigma, init_momemtum_x, init_momentum_y, scalingConstant)
 {
-    let psiRe = new Array(height).fill(0).map(() => new Array(width).fill(0));
-    let psiIm = new Array(height).fill(0).map(() => new Array(width).fill(0));
+    let psiRe = new Array(w*h).fill(0);
+    let psiIm = new Array(w*h).fill(0);
     var maxValRe = 0;
     var maxValIm = 0;
-    for (let y = 0; y < height; y++)
+    for (let x = 0; x < w; x++)
     {
-        for (let x = 0; x < width; x++)
+        for (let y = 0; y < h; y++)
         {
             var initMomentums = math.complex(0, init_momemtum_x * x + init_momentum_y * y);
             var quotient = -((x-center_x)**2 + (y-center_y)**2)/4*sigma**2;
             var exponent = math.exp(math.multiply(math.add(quotient, initMomentums), scalingConstant));
-            psiRe[y][x] = exponent.re;
-            psiIm[y][x] = exponent.im;
+            psiRe[x*h + y] = exponent.re;
+            psiIm[x*h + y] = exponent.im;
             if (exponent.re > maxValRe) {
                 maxValRe = exponent.re;
             }
@@ -68,7 +68,7 @@ function initializeGaussianWavepacket(width, height, center_x, center_y, sigma, 
 
 function initializeEmptyPotential(w, h)
 {
-    return new Array(h).fill(0).map(() => new Array(w).fill(0));
+    return new Array(w * h).fill(0);
 }
 
 function getInitialWaveFunction(w, h)
@@ -81,47 +81,48 @@ function getInitialPotential(w, h)
     return initializeEmptyPotential(w, h);
 }
 
-function calculateLaplacianAtPoint(func, x, y)
+function calculateLaplacianAtPoint(func, w, h, x, y)
 {
-    return (func[x+1][y] - 2*func[x][y] + func[x-1][y] + func[x][y+1] - 2*func[x][y] + func[x][y-1]);
+    return (func[(x+1)*h + y] - 2*func[x*h + y] + func[(x-1)*h + y] + func[x*h + y+1] - 2*func[x*h + y] + func[x*h + y-1]);
 }
 
-function getUpdatedWaveFunction(psi, potential, reducedPlanckConstant, mass, rounding)
+function getUpdatedWaveFunction(psi, w, h, potential, reducedPlanckConstant, mass, rounding)
 {
     var psiRe = psi[0];
     var psiIm = psi[1];
     var delta_t = 0.1;
-    var width = psiRe.length - 1;
-    var height = psiRe[0].length - 1;
     var factor = reducedPlanckConstant * delta_t / (2 * mass);
-    for (let x = 1; x < width; x++)
+    for (let x = 1; x < w - 1; x++)
     {
-        for (let y = 1; y < height; y++)
+        for (let y = 1; y < h - 1; y++)
         {
-            var laplaceRe = calculateLaplacianAtPoint(psiRe, x, y);
-            var laplaceIm = calculateLaplacianAtPoint(psiIm, x, y)
+            var laplaceRe = calculateLaplacianAtPoint(psiRe, w, h, x, y);
+            var laplaceIm = calculateLaplacianAtPoint(psiIm, w, h, x, y);
             if (rounding > -1) {
                 laplaceRe = math.round(laplaceRe, rounding);
                 laplaceIm = math.round(laplaceIm, rounding);
             }
-            psiRe[x][y] = psiRe[x][y] + (-factor) * laplaceIm + potential[x][y] * psiIm[x][y];
-            psiIm[x][y] = psiIm[x][y] + factor * laplaceRe - potential[x][y] * psiRe[x][y];
+            psiRe[x*h + y] = psiRe[x*h + y] + (-factor) * laplaceIm + potential[x*h + y] * psiIm[x*h + y];
+            psiIm[x*h + y] = psiIm[x*h + y] + factor * laplaceRe - potential[x*h + y] * psiRe[x*h + y];
             if (rounding > -1) {
-                psiRe[x][y] = math.round(psiRe[x][y], rounding);
-                psiIm[x][y] = math.round(psiIm[x][y], rounding);
+                psiRe[x*h + y] = math.round(psiRe[x*h + y], rounding);
+                psiIm[x*h + y] = math.round(psiIm[x*h + y], rounding);
             }
         }
-    }    
+    }
+    console.log("Central value = " + psiRe[(w/2)*h + h/2])
     return [psiRe, psiIm];
 }
 
 function Draw(ctx, pixelBuffer, psiRe, psiIm, steps)
 {
-    for (var x = 0; x < ctx.canvas.width; x++)
+    var w = ctx.canvas.width;
+    var h = ctx.canvas.height;
+    for (var x = 0; x < w; x++)
     {
-        for (var y = 0; y < ctx.canvas.height; y++)
+        for (var y = 0; y < h; y++)
         {
-            const probability = psiRe[y][x] ** 2 + psiIm[y][x] ** 2;
+            const probability = psiRe[x*h + y] ** 2 + psiIm[x*h + y] ** 2;
             const r = Math.floor(255 * probability);
             const g = Math.floor(255 * probability);
             const b = 0;
@@ -146,9 +147,9 @@ export function Play(ctx)
         }
         else
         {
-            psi = getUpdatedWaveFunction(psi, potential, 1, 1, -1);
+            psi = getUpdatedWaveFunction(psi, ctx.canvas.width, ctx.canvas.height, potential, 1, 1, -1);
         }
-        console.log(steps)
+        console.log("Steps = " + steps)
         var psiRe = psi[0];
         var psiIm = psi[1];
         Draw(ctx, pixelBuffer, psiRe, psiIm, steps);
